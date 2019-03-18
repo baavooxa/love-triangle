5bb2c6d59f96e6e860875321c8340e6bac97fa4e
Bakur Pompaev <baavooxa@gmail.com>
2019-03-18 14:05:48 +0300

    complete task love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const length = preferences.length; // number of Spichonees in preferences
+  let count = 0;  
+  for (let i = 0; i<length; i++) { 
+      let a = preferences[i] - 1;
+      if (a == i) continue;
+      let b = preferences[a] - 1;
+      if (b == a) continue;
+      let c = preferences[b] - 1;
+      if (c == b) continue;
+      if (c == i) {
+          count++;
+      }
+  }
+  return count / 3; 
 };
